fc20c96127779d77b08dd3b2c56566ec2f2de4b4
ldiego92 <luis.quiros@ucrso.info>
2016-10-04 10:28:06 -0600

    Se agregaron las migraciones de brands, audiovisul_types, lonables, cartographic_material_key_words, books, three_dimensional_object_key_words, audiovisual_material_key_words, cartografiphic_formats, cartographic_materials

---
+++ database/migrations/2016_10_04_013538_create_lonables_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLonablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lonables', function (Blueprint $table) {
+            $table->increments('id');
+            $table->int('barcode');
+            $table->string('note');
+            $table->int('state_id')->unsigned();
+
+            $table->foreign('state_id')->references('id')->on('states');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('lonables');
+    }
+}
