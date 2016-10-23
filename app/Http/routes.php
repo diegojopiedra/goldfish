85252bc0c90cf76220f8eeb86c13c374b600e5fe
JuanCubero <juanelias.cuberom@gmail.com>
2016-10-23 11:35:26 -0600

    Modify routes by Elias

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -10,15 +9,19 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
 Route::get('/', function () {
     return csrf_token();
 });
-
 Route::resource('loan','LoanController');
 Route::resource('users','UserController');
 Route::get('login','UserController@login');
 Route::get('logout','UserController@logout');
+Route::get('loginPrueba', 'UserController@loginPrueba');
+Route::get('logoutPrueba', 'UserController@logoutPrueba');
+Route::group(['middleware' => 'jwt-auth'], function () {
+	Route::post('automatic-loan','LoanController@automaticLoan');
+	Route::get('huhu', 'UserController@huhu');
+});
 Route::get('loan-test', 'LoanController@store');
 Route::post('search-by-identification','UserController@searchByIdentification');
 Route::get('search-by-identification','UserController@searchByIdentification');
@@ -26,31 +29,12 @@ Route::resource('audiovisual-equipment', 'AudiovisualEquipmentController');
 Route::resource('brand','BrandController');
 Route::resource('model','AudiovisualModelController');
 Route::resource('type','TypeController');
-
 Route::resource('cartographic-material','CartographicMaterialController');
-/*Route::get('test-store-carto','CartographicMaterialController@testStore');
-Route::get('test-update-carto/{id}','CartographicMaterialController@testUpdate');
-Route::get('test-destroy-carto/{id}','CartographicMaterialController@testDestroy');*/
-
-
-
 Route::resource('three-dimensional-object','ThreeDimensionalObjectController');
-/*Route::get('test-store-3d','ThreeDimensionalObjectController@testStore');
-Route::get('test-update-3d/{id}','ThreeDimensionalObjectController@testUpdate');
-Route::get('test-destroy-3d/{id}','ThreeDimensionalObjectController@testDestroy');*/
-
-Route::get('loan-by-id','LoanController@returnLoanById');
-Route::get('gets', "LoanController@gets");
 Route::resource('book','BookController');
-
-
-
-
+Route::get('store','UserController@store');
+Route::get('update/{id}','UserController@update');
+Route::get('search-by-name','UserController@searchByName');
 Route::post('return-loan', 'LoanController@returnLoan');
-
-/*Route::get('periodic-publication','PeriodicPublicationController@testStore');
-Route::get('test-destroy-publication/{id}','PeriodicPublicationController@testDestroy');
-
-Route::get('copy-periodic-publication','CopyPeriodicPublicationController@testStore');
-Route::get('copy-test-update/{id}','CopyPeriodicPublicationController@testUpdate');
-Route::get('test-destroy/{id}','CopyPeriodicPublicationController@testDestroy');*/
+Route::resource('periodic-publication','PeriodicPublicationController');
+Route::resource('copy-periodic-publication','CopyPeriodicPublicationController');
